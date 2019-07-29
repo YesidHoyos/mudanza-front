054f3e6fbd2a72e032a4568309b6581c981641f0
Yesid Hoyos <yesidfernandohoyos@gmail.com>
2019-07-28 19:34:26 -0500

    Se realiza el manejo de errores y se genera el archivo de respuesta de la invocación del servicio rest

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,12 @@ import React, {Component} from 'react';
 import MovingForm from './MovingForm';
 import Header from './Header';
 import axios from 'axios';
+import Swal from 'sweetalert2';
 
 class App extends Component{
 
   makeMove = (dni, file) => {
-    
+
     const formData = new FormData();
     formData.append('file', file);
 
@@ -17,10 +18,19 @@ class App extends Component{
     }
     axios.post(`http://localhost:8080/mudanza/viajes/${dni}`, formData, config)
     .then(response => {
-      console.log(response);      
+      const url = window.URL.createObjectURL(new Blob([response.data]));
+      const link = document.createElement('a');
+      link.href = url;
+      link.setAttribute('download', 'file.txt');
+      document.body.appendChild(link);
+      link.click();     
     })
     .catch(error => {
-      console.log(error);      
+      Swal.fire({
+          type: 'error',
+          title: 'Error',
+          text: `${error.response.data}`
+        })      
     })
   }
 
